e7d8645dc142c5370c2f6772e5edce828367335c
yejiebin <yejiebin1126@qq.com>
2019-08-14 11:54:00 +0800

    新增上传图片到云服务ufile

---
+++ src/main/java/com/yjb/controller/FileController.java
@@ -0,0 +1,40 @@
+package com.yjb.controller;
+
+import com.yjb.dto.FileDTO;
+import com.yjb.provider.UCloudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by codedrinker on 2019/6/26.
+ */
+@Controller
+public class FileController {
+    @Autowired
+    private UCloudProvider uCloudProvider;
+
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(HttpServletRequest request) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("editormd-image-file");
+        try {
+            String fileName = uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(1);
+            fileDTO.setUrl(fileName);
+            return fileDTO;
+        } catch (Exception e) {
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(0);
+            fileDTO.setMessage("上传失败");
+            return fileDTO;
+        }
+    }
+}
